637e6cf9353d0cabae6c95b2e2ed76ef8f4341e5
GODWINGZZ <you@example.com>
2022-02-04 11:42:04 +0000

    Task B complete

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,14 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+def stations_within_radius(stations, centre, r):
+    """This function is used for returning a list of station within a certain radius"""
+    Inrange=[]
+    for i in range ( len(stations)):
+        x=stations[i].coord[0]
+        y=stations[i].coord[1]
+        distance=((centre[0]-x)^2+(centre[1]-y)^2)^0.5
+        if distance<=r:
+          Inrange[i]= stations[i]
+
+    return Inrange 
