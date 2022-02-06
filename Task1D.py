5c51167bd9d858ca16670ba2a9c63588ddd24db6
GODWINGZZ <you@example.com>
2022-02-06 02:26:52 +0000

    adad

--- Task1D.py
+++ Task1D.py
@@ -1,6 +1,15 @@
+from re import X
+from typing_extensions import Required
 from floodsystem.geo import rivers_with_station
 from floodsystem.geo import stations_by_river
 from floodsystem.stationdata import build_station_list
+def run():
+ Num=len(rivers_with_station(build_station_list()))
+ OrederedList=sorted(rivers_with_station(build_station_list()))
+ x=OrederedList[0:9]
+ print(Num,'First 10 -',x)
+if __name__ == "__main__":
+    print("*** Task 1E: CUED Part IA Flood Warning System ***")
+    run() 
 
-print(rivers_with_station(build_station_list))
 
