152e8c971a4224ed81871adc0dbabce7d7a6b85a
GODWINGZZ <you@example.com>
2022-02-06 02:27:32 +0000

    dada

--- Task1D.py
+++ Task1D.py
@@ -7,7 +7,7 @@ def run():
  Num=len(rivers_with_station(build_station_list()))
  OrederedList=sorted(rivers_with_station(build_station_list()))
  x=OrederedList[0:9]
- print(Num,'First 10 -',x)
+ print(Num,'stations','First 10 -',x)
 if __name__ == "__main__":
     print("*** Task 1E: CUED Part IA Flood Warning System ***")
     run() 
