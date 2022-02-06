28ad1db130bf6b997cfb42b28d2d338b82b3fa0d
GODWINGZZ <you@example.com>
2022-02-06 01:40:35 +0000

    adsda

---
+++ Task1D.py
@@ -0,0 +1,6 @@
+from floodsystem.geo import rivers_with_station
+from floodsystem.geo import stations_by_river
+from floodsystem.stationdata import build_station_list
+
+print(rivers_with_station(build_station_list))
+
