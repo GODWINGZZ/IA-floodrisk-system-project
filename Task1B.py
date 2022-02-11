3128b38859098ddf07ecc60ecae55ce173fc00a2
mdt41 <mdt41@cam.ac.uk>
2022-02-11 13:54:09 +0000

    added comments

--- Task1B.py
+++ Task1B.py
@@ -5,10 +5,10 @@ from floodsystem.geo import stations_by_distance
 
 
 def run():
-    stations = build_station_list
-    distancefromcam = stations_by_distance(stations,(52.2053, 0.1218))
-    closest10 = distancefromcam[:10]
-    farthest10 = distancefromcam[-10:]
+    stations = build_station_list                                       #imports station data
+    distancefromcam = stations_by_distance(stations,(52.2053, 0.1218))  #input coords p as centre of cambridge
+    closest10 = distancefromcam[:10]                                    #closest 10 entries to cambridge
+    farthest10 = distancefromcam[-10:]                                  #furthest 10 entries from cambridge
     print(closest10,farthest10)
 
 if __name__ == "__main__":
