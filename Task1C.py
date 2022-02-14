b3cb2a07a0ec3601b2e902ddbe280a4c6e17317a
mdt41 <mdt41@cam.ac.uk>
2022-02-14 22:40:31 +0000

    rewrote demonstration code for task 1c

--- Task1C.py
+++ Task1C.py
@@ -1,8 +1,19 @@
 from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
 
-Newlist=[]
-temp1=build_station_list()
-Newlist=stations_within_radius(temp1,(52.2053, 0.1218),10)
+#Newlist=[]
+#temp1=build_station_list()
+#Newlist=stations_within_radius(temp1,(52.2053, 0.1218),10)
 #print(Newlist)
-print(len(Newlist))
+#print(len(Newlist))
+
+#N.B. commented out sam's broken code, morgan's attempt below:
+
+def run():
+    stations = build_station_list                                                   #import list of stations
+    stationsinrange = stations_within_radius(stations,(52.2053, 0.1218),10)         #list of stations within 10km of cambridge
+    print(stationsinrange)
+
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
