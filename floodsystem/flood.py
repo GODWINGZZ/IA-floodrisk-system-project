327cb47c3bc4df5c738261dfaccd35054d23a09f
GODWINGZZ <you@example.com>
2022-03-03 19:35:15 +0000

    ada

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,9 +1,9 @@
 from floodsystem.stationdata import update_water_levels
 from floodsystem.stationdata import build_station_list
 stations=build_station_list()
-update_water_levels(stations)
 
 def stations_level_over_threshold(stations, tol):
+    update_water_levels(stations)
     Tuplelist=()
     for station in stations:
        temp1=station.relative_water_level()
