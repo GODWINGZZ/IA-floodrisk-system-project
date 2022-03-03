67627e418885d389b036677117dccc1e9ec89b7f
GODWINGZZ <you@example.com>
2022-03-03 19:02:47 +0000

    dssda

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -6,7 +6,8 @@ def stations_level_over_threshold(stations, tol):
     update_water_levels(stations)
     Tuplelist=()
     for station in stations:
-       if station.latest_level > tol:
+       temp1=station.relative_water_level(station)
+       if temp1 > tol:
         Retuple=(station.name,station.latest_level)
         Tuplelist=Tuplelist+Retuple
     return Tuplelist
