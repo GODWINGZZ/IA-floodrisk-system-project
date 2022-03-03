08e7c66d809f53cce25c8178b91ace13605cb8f1
GODWINGZZ <you@example.com>
2022-03-03 21:50:42 +0000

    ada

--- Task2B.py
+++ Task2B.py
@@ -5,8 +5,7 @@ def run():
  stations=build_station_list()
  update_water_levels(stations)
  tuplelist=()
- stations_level_over_threshold(stations,0.8)
- for i in stations:
+ for i in stations_level_over_threshold(stations,0.8):
      Retuple=(i.name,i.latest_level)
      tuplelist=tuplelist+Retuple
  print(tuplelist)
