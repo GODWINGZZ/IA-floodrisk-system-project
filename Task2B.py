48ffcf3a82cb38fa6e149c9b8177278ed37e8cd8
GODWINGZZ <you@example.com>
2022-03-03 19:26:16 +0000

    adada

--- Task2B.py
+++ Task2B.py
@@ -1,6 +1,8 @@
 from floodsystem.flood import stations_level_over_threshold
 from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import update_water_levels
 stations=build_station_list()
+update_water_levels(stations)
 def run():
  print(stations_level_over_threshold(stations,0.8))
 
