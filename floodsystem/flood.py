e35feb7a034cdf30a645ae28d713e91d8d0ed071
GODWINGZZ <you@example.com>
2022-03-03 21:29:42 +0000

    adada

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,10 +1,8 @@
 from floodsystem.stationdata import update_water_levels
 from floodsystem.stationdata import build_station_list
 from floodsystem.utils import sorted_by_key
-stations=build_station_list()
-update_water_levels(stations)
 def stations_level_over_threshold(stations, tol):
-    sorted_by_key([(station,station.relative_water_level())
+  return  sorted_by_key([(station,station.relative_water_level())
     for station in stations if station.relative_water_level()!=None and station.relative_water_level()>tol],
     1,
     reverse=True)
