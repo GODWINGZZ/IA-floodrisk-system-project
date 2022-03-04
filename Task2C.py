c6480506e82b202c4b8e25cf8d9f198b9c3b7fe0
GODWINGZZ <you@example.com>
2022-03-04 21:14:45 +0000

    gg

--- Task2C.py
+++ Task2C.py
@@ -2,7 +2,7 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.flood import stations_highest_rel_level
 
 def run():
- stations=build_station_list()
+ stations=build_station_list
  list=stations_highest_rel_level(stations,10)
  print(list)
 
