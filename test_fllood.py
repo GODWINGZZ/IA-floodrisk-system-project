c928bcb3ac59e5920540dcd5ec870c22416f5f96
GODWINGZZ <you@example.com>
2022-03-04 20:48:13 +0000

    aa

---
+++ test_fllood.py
@@ -0,0 +1,6 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.flood import stations_level_over_threshold
+
+def test_stations_level_over_threshold():
+    ins=build_station_list
+    assert type(stations_level_over_threshold(ins,0.8)) is tuple
