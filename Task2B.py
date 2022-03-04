c24ee884d119c1e0ec7c2b74e451f4c805ca5adb
GODWINGZZ <you@example.com>
2022-03-04 02:42:04 +0000

    adad

--- Task2B.py
+++ Task2B.py
@@ -4,11 +4,12 @@ from floodsystem.stationdata import update_water_levels
 def run():
  stations=build_station_list()
  update_water_levels(stations)
- tuplelist=()
+ list=[]
  for i in stations_level_over_threshold(stations,0.8):
-     tuplelist=tuplelist+(i.name,i.latest_level)
- print(tuplelist)
-     
+     temp1=(i[0].name,i[1])
+     list.append(temp1)
+ print(list)
+    
 if __name__ == "__main__":
     print("*** Task 2B: CUED Part IA Flood Warning System ***")
     run()
