f53a71020f18ed6efe61310ec6c797e8e2d99542
GODWINGZZ <you@example.com>
2022-03-04 21:17:01 +0000

    hhh

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -10,6 +10,6 @@ def stations_level_over_threshold(stations, tol):
 def stations_highest_rel_level(stations, N):
   return sorted_by_key(
          [(station,station.relative_water_level())
-         for station in stations if station.relative_water_level()!=None],1,
+         for station in stations if station.relative_water_level()!=None],0,
          reverse=True)[:N]
   
