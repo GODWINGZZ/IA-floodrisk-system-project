32be5b970a3e7d213fb9b4d4d6babdb2c9e64581
GODWINGZZ <you@example.com>
2022-03-05 15:56:12 +0000

    dsada

--- Task2C.py
+++ Task2C.py
@@ -3,7 +3,7 @@ from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import update_water_levels
 
 def run():
- stations=build_station_list
+ stations=build_station_list()
  update_water_levels(stations)
  print(stations_highest_rel_level(stations,10))
  
