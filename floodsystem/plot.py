1bced1a82ec53cbd383f3f1ee89a1400d600db83
GODWINGZZ <you@example.com>
2022-03-06 01:57:07 +0000

    dada

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -4,6 +4,8 @@ import numpy as np
 def plot_water_level_with_fit(station, dates, levels, p):
     plt.plot(dates, levels, '.')
     x1=np.linspace(dates[0],dates[-1],30)
-    plt.plot(x1,polyfit(x1-x1[0]))
+    plt.plot(x1,polyfit(x1-x1[0],levels,p))
+    plt.axhline(y=15)
+    plt.axhline(y=1)
     plt.show
     
