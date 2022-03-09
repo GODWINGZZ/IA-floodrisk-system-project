9f958d62397865591d7c7d6c98c1c4524330e1cb
GODWINGZZ <you@example.com>
2022-03-09 22:02:57 +0000

    sa

--- floodsystem/analyse.py
+++ floodsystem/analyse.py
@@ -1,10 +1,11 @@
 import matplotlib
 import numpy as np
+from datetime import  timedelta
 
 
 def polyfit(dates, levels, p):
     x = matplotlib.dates.date2num(dates)
-    p_coeff = np.polyfit(x[0]-x[1], levels, p)
+    p_coeff = np.polyfit(x-x[0], levels, p)
     poly=np.poly1d(p_coeff)
-    return poly
+    return poly,timedelta(days=x[0])
    
