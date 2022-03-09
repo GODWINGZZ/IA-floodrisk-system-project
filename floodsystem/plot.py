b223bf6d2ffba67fa6e17006488575230947cf3b
GODWINGZZ <you@example.com>
2022-03-09 19:41:22 +0000

    adada

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,9 +1,11 @@
+import matplotlib
 from .analyse import polyfit
 import matplotlib.pyplot as plt
 import numpy as np
 def plot_water_level_with_fit(station, dates, levels, p):
     plt.plot(dates, levels, '.')
-    x1=np.linspace(dates[0],dates[-1],30)
+    x=matplotlib.dates.date2num(dates)
+    x1=np.linspace(x[0],x[-1],30)
     plt.plot(x1,polyfit(x1-x1[0],levels,p))
     plt.axhline(y=15)
     plt.axhline(y=1)
