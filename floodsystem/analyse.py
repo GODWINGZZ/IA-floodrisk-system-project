7d55a8b1f8c48830900e2bac19c9b86cc9fa6bb1
GODWINGZZ <you@example.com>
2022-03-09 21:38:02 +0000

    ada

--- floodsystem/analyse.py
+++ floodsystem/analyse.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def polyfit(dates, levels, p):
     x = matplotlib.dates.date2num(dates)
-    p_coeff = np.polyfit(x, levels, p)
+    p_coeff = np.polyfit(x[0]-x[1], levels, p)
     poly=np.poly1d(p_coeff)
     return poly
    
