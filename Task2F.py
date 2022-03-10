75490c2c42d44a1fb3238f5c69c58a75f65d8a29
mdt41 <mdt41@cam.ac.uk>
2022-03-10 04:59:29 +0000

    fixed some problems with 2f but still isnt fully working

--- Task2F.py
+++ Task2F.py
@@ -2,15 +2,15 @@ from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import update_water_levels,build_station_list
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.plot import plot_water_level_with_fit
-import datetime
+from datetime import timedelta
+
 def run():
     stations=build_station_list()
     update_water_levels(stations)
     temp1=stations_highest_rel_level(stations,5)
     for station in temp1:
        dt=2
-       dates, levels = fetch_measure_levels(station[0].measure_id,
-                                     dt=datetime.timedelta(days=dt))
+       dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days=dt))
        plot_water_level_with_fit(station,dates,levels,4)
 
 
