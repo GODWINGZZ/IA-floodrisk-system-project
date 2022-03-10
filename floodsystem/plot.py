fba84a83fdccd020fd55a17d616323eec496b612
GODWINGZZ <you@example.com>
2022-03-10 17:45:26 +0000

    adaa

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -8,13 +8,10 @@ def plot_water_level_with_fit(station,dates,levels,p):
     #plot_water_levels_no_show(station,dates,levels)
     matplotlib.pyplot.rcdefaults()
     poly,shift=polyfit(dates,levels,p)
-    k=matplotlib.dates.date2num(dates-shift)
-    #num=43
-    ##interval=math.ceil((len(dates)-1)/num)
-    #dates_reduced=systematic_sample(dates[:-1],interval)
-    #dates_reduced.append(dates[-1])
-    #x1=np.array(
-        ###list(map(lambda d:matplotlib.dates.date2num(d-shift),dates_reduced)))
+    k=[]
+    for i in range(len(dates)):
+      k.append(matplotlib.dates.date2num(dates[i]-shift))
+    
         
     plt.plot(k,poly(k),label=f"Best fit, degree {p}",color="red")
     plt.legend
