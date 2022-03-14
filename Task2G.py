cc57b4148eabc25c4d0756d0bcb2260b236f72e3
mdt41 <mdt41@cam.ac.uk>
2022-03-14 10:59:43 +0000

    2g

--- Task2G.py
+++ Task2G.py
@@ -35,7 +35,6 @@ def run():
 
             if poly == 0 or station.latest_level is None or station.typical_range is None: #test for missing data
                 print("no_data")
-
     
             elif polyderivative(1) > 0:
 
